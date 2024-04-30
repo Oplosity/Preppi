923600c1f9bbaa36457c619e9f7c5b5be08401b9
Nikolai <nikolai.zakharov@edu.salpaus.fi>
2024-04-30 13:33:50 +0300

    started working on header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Header from "@/components/header";
 import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <main className="">
+        <Header />
         <Button variant="outline">Button</Button>
     </main>
   );
