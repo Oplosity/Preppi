56d0dcbbad24624ede2d56fcce8e968759b557bf
Nikolai <nikolai.zakharov@edu.salpaus.fi>
2024-05-07 10:10:00 +0300

    changed structure

--- src/app/(auth)/auth/page.tsx
+++ src/app/(auth)/auth/page.tsx
@@ -4,7 +4,6 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <main className="">
-        <Header />
         <Button variant="outline">Button</Button>
     </main>
   );
