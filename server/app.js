7ca1d23025f6733788da322e9c0c233aeaccca29
eduakselhyv <akseli.hyvonen@edu.salpaus.fi>
2024-05-14 12:49:19 +0300

    pushed changes

--- server/app.js
+++ server/app.js
@@ -2,10 +2,42 @@ const express = require('express')
 const app = express()
 const port = 3001
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// User login
+app.post('/users/:type', (req, res) => {
+    const type = req.params.type;
+    const username = req.body.username;
+    const password = req.body.password;
+    const email = req.body.email;
+    let response;
+
+    if (type === "login") {
+        // Function to handle login
+
+    } else if (type === "register") {
+        // Function to handle register
+
+    } else {
+        response = type + " is not a recognized parameter! Did you mean login/register?";
+    }
+    
+    res.send(response);
+})
+
+// quizzes
+app.get('/quizzes/:subject', (req, res) => {
+    const subject = req.params.subject;
+    let subjects = ['math', 'biology', 'english', 'physics', 'chemistry', 'history', 'geography', 'computer science', 'information technology', 'statistics', 'economics', 'accounting', 'business studies', 'foreign languages', 'literature', 'philosophy', 'psychology', 'sociology', 'anthropology', 'linguistics', 'philosophy of science', 'epistemology', 'logic'];
+    let response;
+
+    if (subjects.includes(subject)) {
+        response = 'you chose ' + subject;
+    } else {
+        response = subject + ' is not a recognized parameter! List of accepted parameters: ' + subjects;
+    }
+
+    res.send(response);
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
