24845932f94622990e488bc835e0f19ca2d59cb8
Nikolai <nikolai.zakharov@edu.salpaus.fi>
2024-05-14 12:55:13 +0300

    worked on quizzes list page

--- src/components/quizzesSidebar.tsx
+++ src/components/quizzesSidebar.tsx
@@ -7,22 +7,22 @@ export default function QuizzesSidebar () {
     return(
         <ScrollArea className="w-full h-full bg-[url('/landing/landing-bg.svg')] bg-no-repeat bg-cover standard-app-padding">
             <Button asChild variant="sidebarButton">
-                <Link href="/app/quizzes">All</Link>
+                <Link href="/app/quizzes/all">All</Link>
             </Button>
             <Button asChild variant="sidebarButton">
-                <Link href="/app/quizzes">Link 1</Link>
+                <Link href="/app/quizzes/link1">Link 1</Link>
             </Button>
             <Button asChild variant="sidebarButton">
-                <Link href="/app/quizzes">Link 2</Link>
+                <Link href="/app/quizzes/link2">Link 2</Link>
             </Button>
             <Button asChild variant="sidebarButton">
-                <Link href="/app/quizzes">Link 3</Link>
+                <Link href="/app/quizzes/link3">Link 3</Link>
             </Button>
             <Button asChild variant="sidebarButton">
-                <Link href="/app/quizzes">Link 4</Link>
+                <Link href="/app/quizzes/link4">Link 4</Link>
             </Button>
             <Button asChild variant="sidebarButton">
-                <Link href="/app/quizzes">Link 5</Link>
+                <Link href="/app/quizzes/link5">Link 5</Link>
             </Button>
         </ScrollArea>
     )
