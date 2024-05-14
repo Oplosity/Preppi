24ddea474baae54d94fd653e9635c1904dfd7bcd
Onni Kanerva <onni.kanerva@edu.salpaus.fi>
2024-05-14 12:58:53 +0300

    Slight improvements

--- src/app/(login)/login/page.tsx
+++ src/app/(login)/login/page.tsx
@@ -1,4 +1,3 @@
-import Header from "@/components/header";
 import { Button } from "@/components/ui/button";
 import { Poppins } from "next/font/google";
 import { Input } from "@/components/ui/input";
@@ -7,8 +6,8 @@ const poppins = Poppins({ subsets: ["latin"], weight: "400" });
 
 export default function Home() {
   return (
-    <body className="flex flex-row p-10 overflow-hidden">
-      <main className="flex-2">
+    <div className="flex flex-row overflow-hidden h-screen">
+      <div className="flex-1 place-item-center p-40">
         <h2 className={`${poppins.className} text-black text-2xl xl:text-3xl mb-3 text-pretty`}>Login</h2>
         <h1 className={`${poppins.className} text-black mb-2 text-pretty`}>If you don`t have an account<br/>You can<Button variant="link">Sign up here!</Button></h1>
         <p className={`${poppins.className} text-gray-500 mb-1 text-pretty`}>Email</p>
@@ -16,14 +15,14 @@ export default function Home() {
         <p className={`${poppins.className} text-gray-500 mb-1 text-pretty`}>Password</p>
         <Input className="mb-5"></Input>
         <Button size={"wide"}>Login</Button>
-      </main>
-      <aside className="flex-2 bg-[url('/landing/landing-bg.svg')] bg-cover rounded-xl">
-        <div className="">
+      </div>
+      <div className="flex-3 p-10">
+        <div className="h-full bg-[url('/landing/landing-bg.svg')] bg-cover rounded-xl">
           <Image alt="f" src={'/sign-in-pana.png'} width={600} height={700}/>
           <h1 className={`${poppins.className} text-white text-2xl xl:text-3xl mb-0 text-pretty pl-10`}>Log in to Preppi</h1>
           <p className={`${poppins.className} text-white text-2xl xl:text-2xl mb-3 text-pretty p-10`}> Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
         </div>
-      </aside>
-    </body>
+      </div>
+    </div>
   );
 }
