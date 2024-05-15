0f3ecf651cb3506bc6d6f4b68fb05be7634fe2e7
Onni Kanerva <onni.kanerva@edu.salpaus.fi>
2024-05-15 13:29:55 +0300

    Sign in is now cleaner and updated Sign up

--- src/app/(login)/login/page.tsx
+++ src/app/(login)/login/page.tsx
@@ -12,14 +12,14 @@ export default function Home() {
   return (
     <div className="flex overflow-hidden h-screen">
       <div className="flex flex-col flex-1 justify-center">
-        <Image src="logo-black.svg" height={45} width={157.5} alt="Preppi logo" className="mb-20 ml-[10%]"/>
+        <Link href={"/"}><Image src="logo-black.svg" height={45} width={157.5} alt="Preppi logo" className="mb-20 ml-[10%]"/></Link>
         <div className="self-center">
           <h2 className={`${poppins.className} text-black text-4xl mb-3 text-pretty`}>Login</h2>
           <h1 className={`${poppins.className} text-black text-md mb-2 text-pretty`}>If you don`t have an account<br/>You can <Button variant="link" size={"links"} className="text-md"><Link href={"/signup"}>Sign up here!</Link></Button></h1>
           <p className={`${poppins.className} text-gray-500 mb-1 text-pretty`}>Email</p>
-          <Input></Input>
+          <Input placeholder="Enter your email address"></Input>
           <p className={`${poppins.className} text-gray-500 mb-1 text-pretty`}>Password</p>
-          <Input className="mb-2"></Input>
+          <Input className="mb-2" placeholder="Enter your password"></Input>
           <div className="flex pb-3">
             <Checkbox/>
             <p className={`${poppins.className} text-black text-xs mb-1 text-pretty pl-2`}>Remember me</p>
@@ -47,9 +47,7 @@ export default function Home() {
       
       <div className="flex-1 p-10">
         <div className="flex flex-col h-full w-full bg-[url('/landing/landing-bg.svg')] bg-cover rounded-xl p-10">
-          <div className="grow relative self-center h-full w-full">
-            <Image alt="f" src={'/sign-in-pana.png'} fill/>
-          </div>
+          <div className="grow relative self-center h-full w-full bg-[url('/sign-in-pana.png')] bg-contain bg-no-repeat bg-center"></div>
           <div className="">
             <h1 className={`${poppins.className} text-white text-3xl mb-0 text-pretty pl-10`}>Log in to Preppi</h1>
             <p className={`${poppins.className} text-white text-2xl mb-3 text-pretty pl-10`}>Your personalized quizzes are waiting for you!</p>
--- src/app/(signup)/signup/page.tsx
+++ src/app/(signup)/signup/page.tsx
@@ -12,13 +12,17 @@ export default function Home() {
   return (
     <div className="flex overflow-hidden h-screen">
       <div className="flex flex-col flex-1 justify-center">
-        <Image src="logo-black.svg" height={45} width={157.5} alt="Preppi logo" className="mb-20 ml-[10%]"/>
+        <Link href={"/"}><Image src="logo-black.svg" height={45} width={157.5} alt="Preppi logo" className="mb-20 ml-[10%]"/></Link>
         <div className="self-center">
           <h2 className={`${poppins.className} text-black text-4xl mb-3 text-pretty`}>Sign up</h2>
           <h1 className={`${poppins.className} text-black text-md mb-2 text-pretty`}>If you already have an account registered<br/>You can <Button variant="link" size={"links"} className="text-md"><Link href={"/login"}>Log in here!</Link></Button></h1>
           <p className={`${poppins.className} text-gray-500 mb-1 text-pretty`}>Email</p>
           <Input></Input>
+          <p className={`${poppins.className} text-gray-500 mb-1 text-pretty`}>Username</p>
+          <Input className="mb-2"></Input>
           <p className={`${poppins.className} text-gray-500 mb-1 text-pretty`}>Password</p>
+          <Input></Input>
+          <p className={`${poppins.className} text-gray-500 mb-1 text-pretty`}>Confirm Password</p>
           <Input className="mb-2"></Input>
           <Button size={"wide"}>Register</Button>
         </div>
@@ -26,9 +30,7 @@ export default function Home() {
       
       <div className="flex-1 p-10">
         <div className="flex flex-col h-full w-full bg-[url('/landing/landing-bg.svg')] bg-cover rounded-xl p-10">
-          <div className="grow relative self-center h-full w-full">
-            <Image alt="f" src={'/sign-up-pana.png'} fill/>
-          </div>
+        <div className="grow relative self-center h-full w-full bg-[url('/sign-up-pana.png')] bg-contain bg-no-repeat bg-center"></div>
           <div className="">
             <h1 className={`${poppins.className} text-white text-3xl mb-0 text-pretty pl-10`}>Sign Up to Preppi</h1>
             <p className={`${poppins.className} text-white text-2xl mb-3 text-pretty pl-10`}>Your personalized quizzes are waiting for you!</p>
