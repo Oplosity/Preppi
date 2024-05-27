a6925d779df450706503318c828c0b792dd8c2f4
Onni Kanerva <onni.kanerva@edu.salpaus.fi>
2024-05-27 16:25:59 +0300

    fixed links, the sign in continue options and fixed buton width

--- client/src/app/(authpages)/login/page.tsx
+++ client/src/app/(authpages)/login/page.tsx
@@ -57,10 +57,10 @@ export default function Home() {
   return (
     <div className="flex overflow-hidden h-screen">
       <div className="flex flex-col flex-1 justify-center">
-        <Image src="logo-black.svg" height={45} width={157.5} alt="Preppi logo" className="mb-20 ml-[10%]"/>
+        <Link href={"/"}><Image src="logo-black.svg" height={45} width={157.5} alt="Preppi logo" className="mb-20 ml-[10%]"/></Link>
         <div className="self-center">
           <h2 className={`text-black text-4xl mb-3 text-pretty`}>Login</h2>
-          <h1 className={`text-black text-md mb-2 text-pretty`}>If you don`t have an account<br/>You can <Button variant="link" size={"links"} className="text-md">Sign up here!</Button></h1>
+          <h1 className={`text-black text-md mb-2 text-pretty`}>If you don`t have an account<br/>You can <Button variant="link" size={"links"} className="text-md"><Link href={"/signup"}>Sign up here!</Link></Button></h1>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
               <FormField
@@ -89,22 +89,22 @@ export default function Home() {
                   </FormItem>
                 )}
               />
-              <Button type="submit">Submit</Button>
+              <Button type="submit" size={"wide"}>Submit</Button>
             </form>
           </Form>
           
           <div className="w-full px-24">
             <p className="py-3 text-md text-gray-500 px-1">Or continue with</p>
             <div className="flex gap-3">
-                <Link href="/login/face" className="w-10 h-10">
-                    <FontAwesomeIcon icon={faFacebook} />
-                </Link>
-                <Link href="/login/apple" className="w-8 h-10">
-                    <FontAwesomeIcon icon={faApple} />
-                </Link>
-                <Link href="/login/google" className="w-10 h-1s0">
-                    <FontAwesomeIcon icon={faGoogle} />
-                </Link>
+              <Link href="/login/face">
+                  <FontAwesomeIcon icon={faFacebook} className="h-10 w-10"/>
+              </Link>
+              <Link href="/login/apple">
+                  <FontAwesomeIcon icon={faApple} className="h-10 w-12"/>
+              </Link>
+              <Link href="/login/google">
+                  <FontAwesomeIcon icon={faGoogle} className="h-10 w-10"/>
+              </Link>
             </div>
           </div>
 
@@ -113,7 +113,7 @@ export default function Home() {
       
       <div className="flex-1 p-10">
         <div className="flex flex-col h-full w-full bg-[url('/landing/landing-bg.svg')] bg-cover rounded-xl p-10">
-          <div className="grow relative self-center h-full w-full bg-[url('/sign-up-pana.png')] bg-contain bg-no-repeat bg-center"></div>
+          <div className="grow relative self-center h-full w-full bg-[url('/sign-in-pana.png')] bg-contain bg-no-repeat bg-center"></div>
           <div className="">
             <h1 className={`text-white text-3xl mb-0 text-pretty pl-10`}>Log in to Preppi</h1>
             <p className={`text-white text-2xl mb-3 text-pretty pl-10`}>Your personalized quizzes are waiting for you!</p>
